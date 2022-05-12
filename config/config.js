8d09719ce3d5c48f99aa335729e00ebdfd94f533
Huan <huan.zeng1@gmail.com>
2022-05-12 11:40:08 -0400

    prep for deploy

---
+++ config/config.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    database: 'center',
+    dialect: 'postgres'
+  },
+  test: {
+    database: 'center',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+        require: true
+      }
+    }
+  }
+}
+
+// {
+//   "development": {
+//     "database": "center",
+//     "host": "127.0.0.1",
+//     "dialect": "postgres"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "center",
+//     "host": "127.0.0.1",
+//     "dialect": "postgress"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "center",
+//     "host": "127.0.0.1",
+//     "dialect": "postgress"
+//   }
+// }
